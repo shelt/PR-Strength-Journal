1737dba9e5492925bc06a467693eae16828e5431
Sam Shelton <shelton@dr.com>
2014-10-04 21:23:34 -0500

    Add record-pr page

--- src/ca/samshelton/prsj/RecordPR.java
+++ src/ca/samshelton/prsj/RecordPR.java
@@ -1,7 +1,10 @@
 package ca.samshelton.prsj;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RecordPR extends Activity
 {
@@ -11,7 +14,15 @@ public class RecordPR extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.recordpr);
 		
+		TextView txt = (TextView) findViewById(R.id.bebas_text);
+		Typeface font = Typeface.createFromAsset(getAssets(), "fonts/bebas.ttf");
+		txt.setTypeface(font);
+		txt.setText("TODO");//TODO [Priority]
+		
 		Bundle extras = getIntent().getExtras();
 		String exercise = extras.getString("exercise");
+		
+		//ImageView oval = new ImageView(getBaseContext());
+		//oval.setImageResource(R.drawable.oval);
 	}
 }
